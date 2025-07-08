a3584b339153e8138d99eb9113573d9f9195596a
MauricioAlberto13 <marodriguezg01@outlook.com>
2025-07-08 15:53:35 -0600

    Rol y Municipio del Bl

---
+++ BL/Municipio.cs
@@ -0,0 +1,64 @@
+﻿using DL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Municipio
+    {
+
+
+        private readonly MrodriguezProgramacionNcapasContext _context;
+        //No olvidar inyectar la conexion
+        public Municipio(MrodriguezProgramacionNcapasContext context)
+        {
+            _context = context;
+        }
+
+
+        public ML.Result GetIdMunicipio(int IdEstado)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                var listColonias = _context.VwUsuarioGetAlls.FromSqlRaw($"GetMunicipioByIdEstado {IdEstado}").AsEnumerable()
+                    .ToList();
+
+
+                if (listColonias.Count > 0)
+                {
+                    result.Objects = new List<object>();
+                    foreach (var item in listColonias)
+                    {
+                        ML.Municipio municipio = new ML.Municipio();
+                        // municipio.Estado = new ML.Estado();
+
+                        municipio.IdMunicipio = item.IdMunicipio;
+                        municipio.NombreMunicipio = item.Municipio;
+                        //municipio.Estado.NombreEstado = item.Estado;
+
+                        result.Objects.Add(municipio);
+                    }
+                    result.Correct = true;
+
+                }
+                else
+                {
+                    result.ErrorMessage = "Sin resultados";
+                    result.Correct = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
---
+++ BL/Rol.cs
@@ -0,0 +1,54 @@
+﻿using DL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Rol
+    {
+        private readonly MrodriguezProgramacionNcapasContext _context;
+        //No olvidar inyectar la conexion
+        public Rol(MrodriguezProgramacionNcapasContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public ML.Result GetAll(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                //Se quita el bloque using ya que la conexion ya se encuentra y solo vive en el BL
+                var query = _context.Rols.FromSqlRaw("RolGetAll").ToList();
+                //Recuerdad que entity core no mapea los store procedures y se tienen que mandar a llamar con From SQl Raw en caso de que sea una consulta SELECT
+                //var listUsers = context.UsuarioGetsAllView(usuario.Nombre, usuario.ApellidoPaterno, usuario.ApellidoMaterno, usuario.Rol.IdRol).ToList();
+                result.Objects = new List<object>();
+
+                if (query.Count > 0)
+                {
+                    foreach (var item in query)
+                    {
+                        ML.Rol rol = new ML.Rol();
+                        rol.NombreR = item.Nombre;
+                        rol.IdRol = item.IdRol;
+                        result.Objects.Add(rol);
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
