88c918f1a0fed8f07d12943831e419ecc82f62e8
MauricioAlberto13 <marodriguezg01@outlook.com>
2025-07-25 10:22:47 -0600

    completo

--- ML/Producto.cs
+++ ML/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,16 @@ namespace ML
    public class Producto
     {
         public int IdProducto { get; set; }
+
+        [Required(ErrorMessage = "Ingresa un nombre  es requerido")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\\s]+$", ErrorMessage = "EL nombre no puede numeros")]
+
         public string? Nombre { get; set; }
+        [StringLength(250, ErrorMessage = "Máximo 250 caracteres")]
         public string? Descripcion { get; set; }
+
+        [RegularExpression("^[0-9.]+$", ErrorMessage = "EL precio no puede llevar letras")]
         public decimal? Precio { get; set; }
         public ML.SubCategoria? SubCategoria { get; set; }
         public Byte[]? Imagen { get; set; }
